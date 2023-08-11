392b289a8b3a9b00159df8bfbe4f30cb6c336c3c
DESKTOP-M5LRK9E\ingkle <fkaus113@gmail.com>
2023-08-11 22:14:13 +0900

    플레이어 조작 개발

---
+++ Assets/01_Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 3f;
+    public float jumpForce = 10f;
+
+    private Rigidbody2D _rigidbody2D;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        if (x != 0) {
+            transform.Translate(x * speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            _rigidbody2D.AddForce(Vector2.up * jumpForce);
+        }
+    }
+}
