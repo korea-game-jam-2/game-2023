c84ed7190acee51d88e8ae8d38cc182e2b6c1591
DESKTOP-M5LRK9E\ingkle <fkaus113@gmail.com>
2023-08-12 21:06:44 +0900

    시작 시네머신 제작 및 히트 상태, 무적 상태 추가

--- Assets/01_Scripts/System/GameManager.cs
+++ Assets/01_Scripts/System/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class GameManager : MonoBehaviour
 
     public List<SavePoint> savePoints;
     public PlayerController playerController;
+    public List<PlayableDirector> stageTransition;
 
     private SavePoint _currentSavePoint;
 
@@ -28,14 +30,27 @@ public class GameManager : MonoBehaviour
             _currentSavePoint = savePoints.First();
         }
     }
+    private void Start()
+    {
+        StartGame();
+    }
     public void SetSavePoint(SavePoint savePoint, int order) {
         if (_currentSavePoint.order <= order) { 
             _currentSavePoint = savePoint;
         }
     }
+    public void StartGame() {
+        ShowCinematic(0);
+    }
     public void Respawn() { 
         playerController.ResetState();
         playerController.transform.SetParent(null);
         playerController.transform.position = _currentSavePoint.transform.position;
+    }
+    public void ShowCinematic(int index) {
+        playerController.SetFreeze(true);
+
+        stageTransition[index].stopped += (_)=>playerController.SetFreeze(false);
+        stageTransition[index].Play();
     }
 }
---
+++ Assets/01_Scripts/System/StartStageTransition.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartStageTransition : StageTransition
+{
+    public PlayerController player;
+    public Cinemachine.CinemachineBrain brain;
+    public override void Enter()
+    {
+    }
+
+    public override void Execute()
+    {
+    }
+
+    public override void Exit()
+    {
+    }
+}
