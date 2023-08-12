dafa4f3f80d268cc1b5993785cbc389003d41ef9
dabang906 <dabini906@naver.com>
2023-08-12 18:52:30 +0900

    물약 구현

---
+++ Assets/01_Scripts/Gourd.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gourd : MonoBehaviour
+{
+    PlayerController controller;
+    void Start()
+    {
+        controller = FindObjectOfType<PlayerController>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    
+}
